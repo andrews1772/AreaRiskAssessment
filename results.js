e446f139a242e2d7b25d0010807c5a014764fc1f
Andrew Sanchez <andrews8269@gmail.com>
2020-11-14 17:58:23 -0500

    early crime data implementation

--- results.js
+++ results.js
@@ -12,10 +12,40 @@ let urlParams;
 })();
 
 function getCityState() {
-    document.getElementById("city").innerHTML = urlParams["citySearch"];
-    document.getElementById("state").innerHTML = urlParams["stateSearch"];
+
+    city = urlParams["citySearch"];
+    state = urlParams["stateSearch"];
+
+    document.getElementById("city").innerHTML = city
+    document.getElementById("state").innerHTML = state
+
+    runCrimeAPI(city, state);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     getCityState();
 })
+
+function runCrimeAPI(city, state) {
+    var obj = new Array;
+    var departmentObj = new Array;
+    fetch(`https://api.usa.gov/crime/fbi/sapi/api/agencies/byStateAbbr/${state}?API_KEY=pc8WGc1rAHyDfUlRWhD4f3VVd1Ni2usOnBjteg4N`)
+        .then(response => response.json())
+        .then(data => obj = data)
+        .then(console.log(obj));
+
+
+}
+
+function findDepartment(department) {
+    console.log(department)
+    .then(departmentObj = obj.find(findDepartment))
+    .then(console.log(departmentObj))
+    .then(oriStr = departmentObj.ori)
+    .then(fetch(`https://api.usa.gov/crime/fbi/sapi/api/summarized/agencies/${oriStr}/offenses/2019/2019?API_KEY=pc8WGc1rAHyDfUlRWhD4f3VVd1Ni2usOnBjteg4N`)
+    .then(response => response.json())
+    .then(data => crimeData = data)
+    .then(console.log(crimeData)));;
+
+    return department.name === (city + 'Police Department');
+}
