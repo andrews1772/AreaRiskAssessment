1ecb85833f9a05c36b50358d2825d2d72733064b
Andrew Sanchez <andrews8269@gmail.com>
2020-11-14 16:24:47 -0500

    updated from master

---
+++ results.js
@@ -0,0 +1,21 @@
+let urlParams;
+(window.onpopstate = function () {
+    let match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+function getCityState() {
+    document.getElementById("city").innerHTML = urlParams["citySearch"];
+    document.getElementById("state").innerHTML = urlParams["stateSearch"];
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    getCityState();
+})
