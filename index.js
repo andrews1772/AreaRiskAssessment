23b6a0c328696bf3b776d359ad8e997d791e725d
jnutt99 <36652799+jnutt99@users.noreply.github.com>
2020-11-15 02:31:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-function getCityState() {
-    document.getElementById("city").innerHTML = window.location.search;
-    document.getElementById("state").innerHTML = window.location.search;
-}
-    
 
-        
